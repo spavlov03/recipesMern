19b3e95f9fdb6de6a8578928e3fd16c95d90c24d
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-02-07 15:41:57 -0800

    added socket.io for recipe deletion

--- client/src/components/AllRecipes.jsx
+++ client/src/components/AllRecipes.jsx
@@ -2,11 +2,13 @@ import axios from 'axios'
 import { useState,useEffect } from 'react'
 // import { Link } from 'react-router-dom'
 import CoverFlow from './CoverFlow'
+import {io} from 'socket.io-client'
 
 
 
-const AllRecipes = () => {
+const AllRecipes = ({socket}) => {
   const [recipes,setRecipes] = useState([]); 
+
 
   useEffect(()=>{ 
     const requestOne = axios.get('http://localhost:8000/api/recipes/approved',{withCredentials:true})
@@ -17,6 +19,10 @@ const AllRecipes = () => {
     }))
     .catch(err=>console.log('there is error in useEffect',err))
   },[])
+
+  socket.on('recipeDeleted',(deletedId)=> { 
+    setRecipes(recipes.filter((recipe)=> recipe._id !== deletedId))
+  })
   return (
     <div className='me-5 ms-5'>
       <CoverFlow recipes={recipes}/>
--- server/server.js
+++ server/server.js
@@ -4,35 +4,17 @@ const app = express();
 const PORT = 8000
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
-// const bodyParser = require('body-parser')
-// const fs = require('fs')
-// const path = require('path')
-// const multer = require('multer')
-
-// const storage = multer.diskStorage({
-//   destination: (req,file,cb) => { 
-//     cb(null,'uploads')
-//   },
-//   filename: (req,file,cb) => { 
-//     cb(null, file.fieldname + '-' + Date.now())
-//   }
-// }); 
-// const upload = multer({storage:storage}); 
+const socket = require('socket.io'); 
+const Recipe = require('./models/recipe.model')
 
 require("./config/mongoose.config"); 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.set("view engine", "ejs");
 app.use(cors({
   origin: "http://localhost:3000",credentials:true
 }))
-// app.use(cors({
-//   origin: "https://api.cloudinary.com/"
-// }))
 
 const recipesRoutes = require("./routes/recipes.routes")
 recipesRoutes(app);
@@ -40,8 +22,27 @@ recipesRoutes(app);
 const userRoutes = require("./routes/user.routes")
 userRoutes(app);
 
-
-
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
   console.log(`Server is up and running on port ${PORT}`)
+})
+
+const io = socket(server, { 
+  cors: { 
+    origin: '*', 
+    methods: ['GET','POST'], 
+  }
+}); 
+io.on('connection',(socket) => { 
+  console.log('new user: ',socket.id); 
+  socket.on('deleteRecipe',(payload) => { 
+  Recipe.deleteOne({_id:payload})
+  .then((res)=>{ 
+    io.emit('recipeDeleted',payload)
+  }).catch((err)=> { 
+    console.log(err)
+  })
+  })
+  socket.on('disconnect',(socket)=> {
+    console.log('disconnected socket with id:',socket.id)
+  })
 })
