5ccecd0eae8e7954f2bcfa241d30679f359af865
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-02-08 09:05:36 -0800

    added secret

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb://localhost/recipes',{
+mongoose.connect('mongodb://127.0.0.1/recipes',{
   useNewUrlParser:true,
   useUnifiedTopology:true
 })
