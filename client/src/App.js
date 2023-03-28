a7ec816366b6a45a94e038071b0f82bf31248b19
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-03-27 17:27:05 -0700

    api limit reached

--- client/src/App.js
+++ client/src/App.js
@@ -33,31 +33,32 @@ function App() {
 
   const [recipes,setRecipes] = useState([]); 
   const [apiRec,setApiRec] = useState([]);
-  useEffect(()=> {
-    const options = {
-      method: 'GET',
-      url: 'https://tasty.p.rapidapi.com/recipes/list',
-      params: {from: '0', size: '20', tags: 'under_30_minutes'},
-      headers: {
-        'X-RapidAPI-Key': 'e3d9cc7f6cmshf9a3771f789b43dp106b9ajsne22f3873bb9c',
-        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-      }
-    };
-  axios.request(options).then(function (response) {
-    console.log(response.data.results)
-    setApiRec(response.data.results);
-    // setAllRecipes([...apiRec])
-  }).catch(function (error) {
-    console.error(error);
-  });
-  },[])
+  // useEffect(()=> {
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://tasty.p.rapidapi.com/recipes/list',
+  //     params: {from: '0', size: '20', tags: 'under_30_minutes'},
+  //     headers: {
+  //       'X-RapidAPI-Key': 'e3d9cc7f6cmshf9a3771f789b43dp106b9ajsne22f3873bb9c',
+  //       'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  //     }
+  //   };
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data.results)
+  //   setApiRec(response.data.results);
+  //   // setAllRecipes([...apiRec])
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  // },[])
   useEffect(()=>{ 
     const requestOne = axios.get('http://localhost:8000/api/recipes/approved',{withCredentials:true})
     axios.all([requestOne])
     .then(axios.spread((...res)=>{
       const responseOne = res[0]
-      setRecipes(responseOne.data);
-      setAllRecipes([...recipes,...apiRec])
+      setAllRecipes(responseOne.data);
+      // setRecipes(responseOne.data);
+      // setAllRecipes([...recipes,...apiRec])
     }))
     .catch(err=>console.log('there is error in useEffect',err))
   },[apiRec,recipes])
